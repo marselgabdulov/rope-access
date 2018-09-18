958c5d726c4f4628e863b2db2ffb6717daae0809
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2018-09-18 11:46:32 +0300

    Add hero to index page without form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,47 +2,47 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const Footer = () => (
-  <footer class="footer">
-    <div class="footer__container">
-      <div class="footer__address footer__section">
-        <span class="footer__block-header">Адрес</span>
-        <ul class="footer__items">
-          <li class="footer__item">111033, г. Москва,</li>
-          <li class="footer__item">Золоторожский средний</li>
-          <li class="footer__item">переулок, д. 13</li>
+  <footer className="footer">
+    <div className="footer__container">
+      <div className="footer__address footer__section">
+        <span className="footer__block-header">Адрес</span>
+        <ul className="footer__items">
+          <li className="footer__item">111033, г. Москва,</li>
+          <li className="footer__item">Золоторожский средний</li>
+          <li className="footer__item">переулок, д. 13</li>
         </ul>
       </div>
-      <div class="footer__contacts  footer__section">
-        <span class="footer__block-header">Контакты</span>
-        <ul class="footer__items">
-          <li class="footer__item">
-            <a class="footer__link" href="tel:+74957963303">
+      <div className="footer__contacts  footer__section">
+        <span className="footer__block-header">Контакты</span>
+        <ul className="footer__items">
+          <li className="footer__item">
+            <a className="footer__link" href="tel:+74957963303">
               +7 495 796-33-03
             </a>
           </li>
-          <li class="footer__item">
-            <a class="footer__link" href="tel:+74953909633">
+          <li className="footer__item">
+            <a className="footer__link" href="tel:+74953909633">
               +7 495 390-96-33
             </a>
           </li>
-          <li class="footer__item">
+          <li className="footer__item">
             <a
-              class="footer__link whatsapp"
+              className="footer__link whatsapp"
               href="https://wa.me/79651897920?text=Я%20заинтересован%20в%20заказе%20услуг%20остекления"
             >
               WhatsApp
             </a>
           </li>
-          <li class="footer__item footer__contacts-item">
-            <a class="footer__link" href="mailto:info@vysotnik-msk.ru">
+          <li className="footer__item footer__contacts-item">
+            <a className="footer__link" href="mailto:info@vysotnik-msk.ru">
               info@vysotnik-msk.ru
             </a>
           </li>
         </ul>
       </div>
-      <div class="footer__requisites  footer__section">
-        <span class="footer__block-header">Меню</span>
-        <ul class="footer__items">
+      <div className="footer__requisites  footer__section">
+        <span className="footer__block-header">Меню</span>
+        <ul className="footer__items">
           <li className="footer__item">
             <Link to="/" className="footer__link">
               Главная
@@ -70,15 +70,15 @@ const Footer = () => (
           </li>
         </ul>
       </div>
-      <div class="footer__working-time  footer__section">
-        <span class="footer__block-header">Время работы</span>
-        <ul class="footer__items">
+      <div className="footer__working-time  footer__section">
+        <span className="footer__block-header">Время работы</span>
+        <ul className="footer__items">
           <li>Ежедневно: 9:00—20:00</li>
         </ul>
       </div>
 
-      <div class="footer__admin-links  footer__section">
-        <ul class="footer__items">
+      <div className="footer__admin-links  footer__section">
+        <ul className="footer__items">
           <li className="footer__item">
             <Link to="/admin" className="footer__link">
               техподдержка
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,36 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link } from 'gatsby-link'
 
 import Layout from '../components/layout'
+import '../styles/index.css'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="hero">
+      <div className="hero__container">
+        <div className="hero__info">
+          <span className="hero__title">Остекление</span>
+          <br />
+          <span className="hero__title-secondary">
+            Замена, монтаж и обслуживание
+          </span>
+          <br />
+          <span className="hero__title-secondary">
+            Работаем по всей Москве и области
+          </span>
+          <br />
+          <span className="hero__title-garanty">Гарантия от 2-х лет</span>
+        </div>
+        <div className="hero__contact">
+          <a className="hero__link" href="tel:+74957963303">
+            +7 495 796-33-03
+          </a>
+          <a className="hero__link" href="mailto:info@vysotnik-msk.ru">
+            info@vysotnik-msk.ru
+          </a>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
