9f8accd0cf29135fe0a7248995934024f7202712
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2018-09-20 12:59:42 +0300

    Add typewriter without styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import ModalComponent from '../components/ModalComponent'
+import { Typewriter } from '../components/Typewriter'
 import '../styles/index.css'
 
 const IndexPage = () => (
@@ -34,6 +35,10 @@ const IndexPage = () => (
         </div>
       </div>
     </div>
+    <Typewriter
+      className="title-desc"
+      texts={['bread.', 'buns.', 'balls.', 'rolls.', 'puffs.', 'goodness.']}
+    />
   </Layout>
 )
 
