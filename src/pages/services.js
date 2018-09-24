0a734c8bf8b2c110db1cf5e006f5928ab24b5497
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2018-09-24 14:00:36 +0300

    Make redirection from index page to definite id in services page

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react'
+import scrollToElement from 'scroll-to-element'
 
 import Layout from '../components/layout/layout'
 
 class ServicesPage extends Component {
   componentDidMount() {
-    // Decode entities in the URL
-    // Sometimes a URL like #/foo#bar will be encoded as #/foo%23bar
-    window.location.hash = window.decodeURIComponent(window.location.hash)
-    const scrollToAnchor = () => {
-      const hashParts = window.location.hash.split('#')
-      if (hashParts.length > 2) {
-        const hash = hashParts.slice(-1)[0]
-        document.querySelector(`#${hash}`).scrollIntoView()
-      }
-    }
-    scrollToAnchor()
-    window.onhashchange = scrollToAnchor
+    const element = '#' + window.location.href.split('#')[1]
+    console.log(element)
+    scrollToElement(element, {
+      offset: 0,
+      ease: 'outQuad',
+      duration: 500,
+    })
   }
   render() {
     return (
